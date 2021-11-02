09a2d6bd7123007ab7add5142c0c70c29be391fe
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-11-02 11:22:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -20,20 +16,17 @@ def sigmoid_gradient(z):
 
 if __name__ == '__main__':
     data = loadmat('ex4data1.mat')
-    print(data)
+    # print(data)
 
-    X = data['X']
-    y = data['y']
-    print(X.shape, y.shape)
+    X = data['X']  # (5000, 400)
+    y = data['y']  # (5000, 1)
+    # print(X.shape, y.shape)
 
-    print(sigmoid_gradient(0))
+    weight = loadmat('ex4weights.mat')
+    theta1, theta2 = weight['Theta1'], weight['Theta2']  # (25, 401) (10, 26)
+    # print(theta1.shape, theta2.shape)
 
-
-    def sigmoid_gradient(z):
-        return np.multiply(sigmoid(z), (1 - sigmoid(z)))
-
+    # print(sigmoid_gradient(0)) #0.25
 
     # np.random.random(size) 返回size大小的0-1随机浮点数
-    params = (np.random.random(size=hidden_size * (input_size + 1) + num_labels * (hidden_size + 1)) - 0.5) * 0.24
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # params = (np.random.random(size=hidden_size * (input_size + 1) + num_labels * (hidden_size + 1)) - 0.5) * 0.24
