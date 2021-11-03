f61aa52cf23c19cf309090163c8fd921186bc782
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-11-03 14:10:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 from scipy.io import loadmat
+from sklearn.metrics import classification_report
 from sklearn.preprocessing import OneHotEncoder
+from scipy.optimize import minimize
 
 
 def sigmoid(z):
@@ -70,6 +72,105 @@ def costReg(theta1, theta2, input_size, hidden_size, num_labels, X, y, learning_
     return J
 
 
+def backprop(params, input_size, hidden_size, num_labels, X, y, learning_rate):
+    m = X.shape[0]
+    X = np.matrix(X)
+    y = np.matrix(y)
+
+    a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
+
+    theta1 = np.matrix(np.reshape(params[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1))))
+    theta2 = np.matrix(np.reshape(params[hidden_size * (input_size + 1):], (num_labels, (hidden_size + 1))))
+
+    J = 0
+    delta1 = np.zeros(theta1.shape)
+    delta2 = np.zeros(theta2.shape)
+
+    for i in range(m):
+        first_term = np.multiply(-y[i, :], np.log(h[i, :]))
+        second_term = np.multiply((1 - y[i, :]), np.log(1 - h[i, :]))
+        J += np.sum(first_term - second_term)
+
+    J = J / m
+
+    for t in range(m):
+        a1t = a1[t, :]  # (1, 401)
+        z2t = z2[t, :]  # (1, 25)
+        a2t = a2[t, :]  # (1, 26)
+        ht = h[t, :]  # (1, 10)
+        yt = y[t, :]  # (1, 10)
+
+        d3t = ht - yt  # (1, 10)
+
+        z2t = np.insert(z2t, 0, values=np.ones(1))  # (1, 26)
+        d2t = np.multiply((theta2.T * d3t.T).T, sigmoid_gradient(z2t))  # (1, 26)
+
+        delta1 = delta1 + (d2t[:, 1:]).T * a1t
+        delta2 = delta2 + d3t.T * a2t
+
+    delta1 = delta1 / m
+    delta2 = delta2 / m
+
+    return J, delta1, delta2
+
+
+def backpropReg(params, input_size, hidden_size, num_labels, X, y, learning_rate):
+    m = X.shape[0]
+    X = np.matrix(X)
+    y = np.matrix(y)
+
+    # reshape the parameter array into parameter matrices for each layer
+    theta1 = np.matrix(np.reshape(params[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1))))
+    theta2 = np.matrix(np.reshape(params[hidden_size * (input_size + 1):], (num_labels, (hidden_size + 1))))
+
+    # run the feed-forward pass
+    a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
+
+    # initializations
+    J = 0
+    delta1 = np.zeros(theta1.shape)  # (25, 401)
+    delta2 = np.zeros(theta2.shape)  # (10, 26)
+
+    # compute the cost
+    for i in range(m):
+        first_term = np.multiply(-y[i, :], np.log(h[i, :]))
+        second_term = np.multiply((1 - y[i, :]), np.log(1 - h[i, :]))
+        J += np.sum(first_term - second_term)
+
+    J = J / m
+
+    # add the cost regularization term
+    J += (float(learning_rate) / (2 * m)) * (np.sum(np.power(theta1[:, 1:], 2)) + np.sum(np.power(theta2[:, 1:], 2)))
+
+    # perform backpropagation
+    for t in range(m):
+        a1t = a1[t, :]  # (1, 401)
+        z2t = z2[t, :]  # (1, 25)
+        a2t = a2[t, :]  # (1, 26)
+        ht = h[t, :]  # (1, 10)
+        yt = y[t, :]  # (1, 10)
+
+        d3t = ht - yt  # (1, 10)
+
+        z2t = np.insert(z2t, 0, values=np.ones(1))  # (1, 26)
+        d2t = np.multiply((theta2.T * d3t.T).T, sigmoid_gradient(z2t))  # (1, 26)
+
+        delta1 = delta1 + (d2t[:, 1:]).T * a1t
+        delta2 = delta2 + d3t.T * a2t
+
+    delta1 = delta1 / m
+    delta2 = delta2 / m
+
+    # add the gradient regularization term
+    delta1[:, 1:] = delta1[:, 1:] + (theta1[:, 1:] * learning_rate) / m
+    delta2[:, 1:] = delta2[:, 1:] + (theta2[:, 1:] * learning_rate) / m
+
+    # unravel the gradient matrices into a single array
+    grad = np.concatenate((np.ravel(delta1), np.ravel(delta2)))
+
+    return J, grad
+
+
 if __name__ == '__main__':
     data = loadmat('ex4data1.mat')
     # print(data)
@@ -97,7 +198,31 @@ if __name__ == '__main__':
     # print(y_onehot.shape)
     print(y[0], y_onehot[0, :])  # [10] [0. 0. 0. 0. 0. 0. 0. 0. 0. 1.]
 
-    # print(sigmoid_gradient(0)) #0.25
+    input_size = 400
+    hidden_size = 25
+    num_labels = 10
+    learning_rate = 1
+
+    print(cost(theta1, theta2, input_size, hidden_size, num_labels, X, y_onehot, learning_rate))  # 0.2876291651613188
+
+    print(sigmoid_gradient(0))  # 0.25
 
     # np.random.random(size) 返回size大小的0-1随机浮点数
-    # params = (np.random.random(size=hidden_size * (input_size + 1) + num_labels * (hidden_size + 1)) - 0.5) * 0.24
+    params = (np.random.random(size=hidden_size * (input_size + 1) + num_labels * (hidden_size + 1)) - 0.5) * 0.24
+
+    # minimize the objective function
+    fmin = minimize(fun=backpropReg, x0=(params),
+                    args=(input_size, hidden_size, num_labels, X, y_onehot, learning_rate),
+                    method='TNC', jac=True, options={'maxiter': 250})
+    print(fmin)
+
+    X = np.matrix(X)
+    thetafinal1 = np.matrix(np.reshape(fmin.x[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1))))
+    thetafinal2 = np.matrix(np.reshape(fmin.x[hidden_size * (input_size + 1):], (num_labels, (hidden_size + 1))))
+
+    # 计算使用优化后的θ得出的预测
+    a1, z2, a2, z3, h = forward_propagate(X, thetafinal1, thetafinal2)
+    y_pred = np.array(np.argmax(h, axis=1) + 1)
+    print(y_pred)
+
+    print(classification_report(y, y_pred))
